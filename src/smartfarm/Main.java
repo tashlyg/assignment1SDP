b90fc7267e0e07cc1fc258c65e3abcb71c05b0b1
tashlyg <tashlygg@gmail.com>
2026-09-13 20:27:21 +0500

    client

--- src/smartfarm/Main.java
+++ src/smartfarm/Main.java
@@ -2,6 +2,22 @@ package smartfarm;
 
 public class Main {
     static void main() {
+        System.out.println("1. Smart Farm Configuration");
+        SmartFarm highTechFarm = new SmartFarm.Builder(
+                "Greenotech Alpha",
+                "Tomatoes",
+                12.5,
+                new Location("North", 67)
+        ).applyHighTechPreset().setTemperature(24.5).build();
 
+        System.out.println(highTechFarm);
+        System.out.println("\n2. Custom Tropical Farm Configuration");
+        SmartFarm tropicalFarm = new SmartFarm.Builder(
+                "Tropico Greenhouse",
+                "Cucumbers",
+                5.0,
+                new Location("South", 12)
+        ).applyTropicalClimatePreset().enableAutoIrrigation().setSensorInterval(10).build();
+        System.out.println(tropicalFarm);
     }
 }
