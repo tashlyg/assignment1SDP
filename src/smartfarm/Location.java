662163d03c83acade26e440bc53729bfd42b2fbc
tashlyg <tashlygg@gmail.com>
2026-09-13 18:51:28 +0500

    location

--- src/smartfarm/Location.java
+++ src/smartfarm/Location.java
@@ -1,5 +1,21 @@
 package smartfarm;
 
 public class Location {
+    private final String sector;
+    private final int greenhouseNumber;
 
+    public Location(String sector, int greenhouseNumber) {
+        this.sector = sector;
+        this.greenhouseNumber = greenhouseNumber;
+    }
+    public String getSector() {
+        return sector;
+    }
+    public int getGreenhouseNumber() {
+        return greenhouseNumber;
+    }
+    @Override
+    public String toString() {
+        return "Sector=" + sector + ", Greenhouse=" + greenhouseNumber;
+    }
 }
